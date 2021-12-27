857ed4b0a003a86a1a38a7bd82aad719b1151387
Abdulmalek Al-Gahmi <aalgahmi@gmail.com>
2021-12-27 07:20:00 -0700

    Fixed css/sass url problem

--- webpack.config.js
+++ webpack.config.js
@@ -40,10 +40,10 @@ module.exports = {
         use: ['babel-loader']
       }, {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+        use: [MiniCssExtractPlugin.loader, {loader: 'css-loader', options: {url: false}}, 'postcss-loader']
       }, {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+        use: [MiniCssExtractPlugin.loader, {loader: 'css-loader', options: {url: false}}, 'postcss-loader', 'sass-loader']
       }, {
         test: /\.(json|txt|dat|png|jpg|jpeg|gif|svg|eot|ttf|woff|woff2)$/i,
         use: [{
